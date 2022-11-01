968ac15021c335b015f6cf4ed0b5b8be43ab99d7
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2022-11-01 15:18:01 +0100

    enable editing of individual events

--- src/app/store.js
+++ src/app/store.js
@@ -19,5 +19,30 @@ export const store = {
     submitEvent(eventDetails) {
         const activeDay = this.getActiveDay();
         activeDay.events.push({"details": eventDetails, "edit": false});
+    },
+
+    editEvent(dayId, eventDetails) {
+        this.resetEditOfAllEvents();
+        const eventObj = this.getEventObj(dayId, eventDetails);
+        eventObj.edit = true;
+    },
+    
+    resetEditOfAllEvents() {
+        this.state.data.forEach(dayObj => {
+            dayObj.events.forEach(event => {
+                event.edit = false;
+            });
+        });
+    },
+    
+    updateEvent(dayId, originalEventDetails, newEventDetails) {
+        const eventObj = this.getEventObj(dayId, originalEventDetails);
+        eventObj.details = newEventDetails;
+        eventObj.edit = false;
+    },
+
+    getEventObj(dayId, eventDetails) {
+        const dayObj = this.state.data.find(day => day.id === dayId);
+        return dayObj.events.find(event => event.details === eventDetails);
     }
 }
